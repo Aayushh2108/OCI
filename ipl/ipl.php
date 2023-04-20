b036a9df11d2141261d6c7e0120a0ed35c85aa8e
Ayush <119883228+Aayushh2108@users.noreply.github.com>
2023-04-21 00:39:54 +0530

    Add files via upload

---
+++ ipl/ipl.php
@@ -0,0 +1,313 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IPL</title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+
+    <link rel="stylesheet" href="../style2.css">
+</head>
+<body>
+<header class="header">
+		<h1 class="logo"><a href="../welcome.php">OCI</a></h1>
+      <ul class="main-nav">
+          <li><a href="../welcome.php">Home</a></li>
+          <li><a href="../about.php">About</a></li>
+          <li><a href="../contact.php">Contact</a></li>
+          <li><a href="../logout.php">Logout</a></li>
+      </ul>
+	</header>
+  <h1 class="title">IPL Information</h1>
+  <p><b>Gujarat Titans (GT) won Tata IPL 2022 title by beating Rajasthan Royals (RR) on 29th May 2022 and here we are providing the IPL winners list from 2008 to 2022.</b>
+    </p>
+
+    <p>The Indian Premier League began in 2008, a year after the Indian cricket team won the first edition of the World T 20 against Pakistan in a thrilling final. 
+    What started as an ambitious project 15 years ago, is now considered the best Twenty 20 league in the world. Mumbai Indians team is the most successful IPL team,
+     having won the tournament five times and having finished runners-up on one occasion.
+     Chennai Super Kings is the other team to have lifted the IPL title on four occasions while they have finished second once.
+    </p>
+
+    <h1>IPL Winners List from 2008 to 2022</h1>
+    <p>Here, we present the list of IPL winners in the table which includes winner, runner-up, venue, number of teams that participated, 
+        player of the match and player of the series. Let's have look at the table of IPL Winners List from 2008 to 2022.
+    </p>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>IPL Winner Team</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2008</td>
+                <td>Rajasthan Royals</td>
+            </tr>
+            <tr>
+                <td>2009</td>
+                <td>Deccan Chargers</td>
+            </tr>
+            <tr>
+                <td>2010</td>
+                <td>Chennai Super Kings</td>
+            </tr>
+            <tr>
+                <td>2011</td>
+                <td>Chennai Super Kings</td>
+            </tr>
+            <tr>
+                <td>2012</td>
+                <td>Kolkata Knight Riders</td>
+            </tr>
+            <tr>
+                <td>2013</td>
+                <td>Mumbai Indians</td>
+            </tr>
+            <tr>
+                <td>2014</td>
+                <td>Kolkata Knight Riders</td>
+            </tr>
+            <tr>
+                <td>2015</td>
+                <td>Mumbai Indians</td>
+            </tr>
+            <tr>
+                <td>2016</td>
+                <td>Sunrisers Hyderabad</td>
+            </tr>
+            <tr>
+                <td>2017</td>
+                <td>Mumbai Indians</td>
+            </tr>
+            <tr>
+                <td>2018</td>
+                <td>Chennai Super Kings</td>
+            </tr>
+            <tr>
+                <td>2019</td>
+                <td>Mumbai Indians</td>
+            </tr>
+            <tr>
+                <td>2020</td>
+                <td>Mumbai Indians</td>
+            </tr>
+            <tr>
+                <td>2021</td>
+                <td>Chennai Super Kings</td>
+            </tr>
+            <tr>
+                <td>2022</td>
+                <td>Gujarat Titans</td>
+            </tr>
+        </tbody>
+    </table>
+
+<h1>TATA IPL Winner 2022</h1>
+<p><b>Tata IPL 2022 final match was played between Gujarat Titans (GT) vs Rajasthan Royals (RR) on 29th May 2022 and Gujarat Titans won the IPL 2022 final match by 7 wickets. 
+    Rajasthan Royals (RR) won the toss and elected bat first. RR has scored 130/9 in 20 overs. 
+</b> Yuzvendra Chahal became the highest wicket-taker in Tata IPL 2022. </p>
+
+<h1>IPL Winners List (Table)</h1>
+<p>The detailed analysis of IPL Winner List, Runner Up, Venue, Number of teams, Player of the Match and Player of the Series are given below:</p>
+
+<table>
+    <tbody>
+        <tr>
+            <td><strong>Year</strong></td>
+            <td><strong>Winner</strong></td>
+            <td><strong>Runner Up</strong></td>
+            <td><strong>Venue</strong></td>
+            <td><strong>Number of teams</strong></td>
+            <td><strong>Player of the Match</strong></td>
+        </tr>
+        <tr>
+            <td>2022</td>
+            <td>Gujarat Titans</td>
+            <td>Rajasthan Royals&nbsp;</td>
+            <td>Ahmedabad</td>
+            <td>10</td>
+            <td>--</td>
+        </tr>
+        <tr>
+            <td>2021</td>
+            <td>Chennai Super Kings</td>
+            <td>Kolkata Knight Riders</td>
+            <td>Dubai</td>
+            <td>8</td>
+            <td>Faf du Plessis</td>
+        </tr>
+        <tr>
+            <td>2020</td>
+            <td>Mumbai Indians</td>
+            <td>Delhi Capitals</td>
+            <td>Dubai</td>
+            <td>8</td>
+            <td>Trent Boult</td>
+        </tr>
+        <tr>
+            <td>2019</td>
+            <td>Mumbai Indians</td>
+            <td>Chennai Super Kings</td>
+            <td>Hyderabad</td>
+            <td>8</td>
+            <td>Jasprit Bumrah</td>
+        </tr>
+        <tr>
+            <td>2018</td>
+            <td>Chennai Super Kings</td>
+            <td>Sunrisers Hyderabad</td>
+            <td>Mumbai</td>
+            <td>8</td>
+            <td>Shane Watson</td>
+        </tr>
+        <tr>
+            <td>2017</td>
+            <td>Mumbai Indians</td>
+            <td>Rising Pune Supergiants</td>
+            <td>Hyderabad</td>
+            <td>8</td>
+            <td>Krunal Pandya</td>
+        </tr>
+        <tr>
+            <td>2016</td>
+            <td>Sunrisers Hyderabad</td>
+            <td>Royal Challengers Bangalore</td>
+            <td>Bangalore</td>
+            <td>8</td>
+            <td>Ben Cutting</td>
+        </tr>
+        <tr>
+            <td>2015</td>
+            <td>Mumbai Indians</td>
+            <td>Chennai Super Kings</td>
+            <td>Kolkata</td>
+            <td>8</td>
+            <td>Rohit Sharma</td>
+        </tr>
+        <tr>
+            <td>2014</td>
+            <td>Kolkata Knight Riders</td>
+            <td>Kings XI Punjab</td>
+            <td>Bangalore</td>
+            <td>8</td>
+            <td>Manish Pandey</td>
+        </tr>
+        <tr>
+            <td>2013</td>
+            <td>Mumbai Indians</td>
+            <td>Chennai Super Kings</td>
+            <td>Kolkata</td>
+            <td>9</td>
+            <td>Kieron Pollard</td>
+        </tr>
+        <tr>
+            <td>2012</td>
+            <td>Kolkata Knight Riders</td>
+            <td>Chennai Super Kings</td>
+            <td>Chennai</td>
+            <td>9</td>
+            <td>Manvinder Bisla</td>
+        </tr>
+        <tr>
+            <td>2011</td>
+            <td>Chennai Super Kings</td>
+            <td>Royal Challengers Bangalore</td>
+            <td>Chennai</td>
+            <td>10</td>
+            <td>Murali Vijay</td>
+        </tr>
+        <tr>
+            <td>2010</td>
+            <td>Chennai Super Kings</td>
+            <td>Mumbai Indians</td>
+            <td>Mumbai</td>
+            <td>8</td>
+            <td>Suresh Raina</td>
+        </tr>
+        <tr>
+            <td>2009</td>
+            <td>Deccan Chargers</td>
+            <td>Royal Challengers Bangalore</td>
+            <td>Johhanesburg</td>
+            <td>8</td>
+            <td>Anil Kumble</td>
+        </tr>
+        <tr>
+            <td>2008</td>
+            <td>Rajasthan Royals</td>
+            <td>Chennai Super Kings</td>
+            <td>Mumbai</td>
+            <td>8</td>
+            <td>Yusuf Pathan</td>
+        </tr>
+    </tbody>
+</table>
+
+<h1>Most IPL Winner</h1>
+<p>Chennai Super Kings is the best team to have lifted the IPL title on four occasions. Let's have a look at the most IPL winners list by a team.</p>
+
+<table>
+    <tbody>
+        <tr>
+            <td><strong>IPL Winner Team</strong></td>
+            <td><strong>Times</strong></td>
+            <td><strong>Year</strong></td>
+        </tr>
+        <tr>
+            <td>Mumbai Indians</td>
+            <td>5 times</td>
+            <td>2013, 2015,&nbsp;2017,&nbsp;2019, 2020</td>
+        </tr>
+        <tr>
+            <td>Chennai Super Kings</td>
+            <td>4 times</td>
+            <td>2010, 2011, 2018, 2021</td>
+        </tr>
+        <tr>
+            <td>Kolkata Knight Riders</td>
+            <td>2 times</td>
+            <td>2012, 2014</td>
+        </tr>
+        <tr>
+            <td>Sunrisers Hyderabad</td>
+            <td>1 time</td>
+            <td>2016</td>
+        </tr>
+        <tr>
+            <td>Rajasthan Royals</td>
+            <td>1 time</td>
+            <td>2008</td>
+        </tr>
+        <tr>
+            <td>Deccan Chargers</td>
+            <td>1 time</td>
+            <td>2009</td>
+        </tr>
+    </tbody>
+</table>
+
+<footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="container">
+        <span>Copyright © 2022 <a class="grey-text text-lighten-4" href="#" target="_blank">OCI</a> All rights reserved. Design and Developed by <a class="grey-text text-lighten-4" href="#">OCI</a></span>
+        
+      </div>
+    </div>
+</footer>
+
+<!-- partial -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.1/vue.min.js'></script><script  src="../script.js"></script>
+</body>
+</html>
