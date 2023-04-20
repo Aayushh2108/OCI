78835a4577ca2cf4e8fe6712f07f9bdcade979c8
Ayush <119883228+Aayushh2108@users.noreply.github.com>
2023-04-20 23:18:31 +0530

    Add files via upload

---
+++ news/news.php
@@ -0,0 +1,140 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News</title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+
+    <link rel="stylesheet" href="../style2.css">
+</head>
+<body>
+<header class="header">
+		<h1 class="logo"><a href="../welcome.php">OCI</a></h1>
+      <ul class="main-nav">
+          <li><a href="../welcome.php">Home</a></li>
+          <li><a href="../about.php">About</a></li>
+          <li><a href="../contact.php">Contact</a></li>
+          <li><a href="../logout.php">Logout</a></li>
+      </ul>
+	</header>
+  <h1 class="title">News</h1>
+
+  <h1>Cricket News and Editorials</h1>
+
+<h2>India look to expand batting blueprint to ODIs against England</h2>
+
+<p>The hosts will be strengthened the return of Ben Stokes, Joe Root and Jonny Bairstow, and hope to start the ODI series on a better note than T20Is</p>
+
+<section class="cb-nws-dtl-itms" itemprop="articleBody"> <p class="cb-nws-para"> With matches coming thick and fast, a week can seem like a long time.
+     On July 5, England had their epoch-making moment in Test cricket by overhauling a target of 378 to secure parity in the series against India. 
+     Amidst England's renaissance of sorts in the longest format, they have been balancing on a tightrope in limited overs cricket. At the start of the year,
+      England slipped to a series loss in the shortest format versus the West Indies.
+       Just a few days ago, India clipped England's wings further by sealing the three-match T20I series at the same ground where the hosts had chased down 378. </p> </section>
+
+      <p> Due to the packed schedule and injuries, England haven't always been able to field their best XI and also their charismatic captain has decided to hang up his boots from international cricket. 
+        As India and England travel to London to play the first ODI, the 50-over champions would be confident that despite a few malfunctioning nuts and bolts, the base of their edifice structure is still strong.</p>
+
+        <p class="cb-nws-para"> <b>When:</b> England vs India, 1st ODI, July 12, 1 PM Local, 5.30 PM IST </p>
+
+    <section class="cb-nws-dtl-itms" itemprop="articleBody"> <p class="cb-nws-para"> <b>Where:</b>The Oval </p></section>
+
+<section class="cb-nws-dtl-itms" itemprop="articleBody"> <p class="cb-nws-para"> <b>What to expect:</b> Generally, the decks at The Oval offer some bounce. Incidentally, since 2017, 
+    the team batting first has found enough success (seven wins to six losses) at the Oval in ODIs. Barring a few clouds on the horizon, the weather forecast predicts largely a clear day. </p> 
+</section>
+
+
+
+<h2>Former NZ captain Barry Sinclair passes away aged 85</h2>
+
+<section class="cb-nws-dtl-itms" itemprop="articleBody"> <p class="cb-nws-para"> Barry Sinclair, the former New Zealand cricketer, has passed away at the age of 85.
+     Sinclair, who captained New Zealand in three Test matches, was the third player from the country to score 1000 runs after Bert Sutcliffe and John R Reid. </p> 
+</section>
+
+     <section class="cb-nws-dtl-itms" itemprop="articleBody"> <p class="cb-nws-para"> Sinclair played 21 Tests between 1963 and 1968, scoring 1148 runs. 
+        He also struck three centuries - one apiece against South Africa, Pakistan and England. Sinclair featured in a total of 118 first-class games, 
+        scoring 6114 runs with six hundreds and 38 fifties. </p> </section>
+
+        <section class="cb-nws-dtl-itms" itemprop="articleBody"> <p class="cb-nws-para"> Sinclair was involved with the New Zealand Cricket Players Association, 
+            becoming the inaugural patron in 2008-09. </p> 
+        </section>
+
+<h2>Zimbabwe trounce Singapore in tournament opener, USA ease past Jersey</h2>
+
+    <p>Hosts Zimbabwe smashed Singapore by a record 111 runs in their opening match at the T20 World Cup Global Qualifier B at Queens Club, Bulawayo.
+     Sikandar Raza and Sean Williams struck rapid half-centuries as Zimbabwe racked up up 236/5 before restricting Singapore to 127/7 to secure two points
+      and a healthy net run rate to go top of Group A.
+    </p>
+
+
+    <p>Meanwhile, at Bulawayo Athletic Club, the USA recorded a convincing win over Jersey, though a potential injury to star quick Ali Khan might yet dampen their celebrations.
+     Teenaged Jersey bat Asa Tribe and Ben Ward lifted the Channel Islanders to 154/5 after a shaky start to the innings but the total proved insufficient in the face of an in-form Steven Taylor,
+     who made full use of two early reprieves to see his side comfortably home, sending the final ball high over long on to seal the win - and a maiden T20I century - with two overs to spare.
+    </p>
+
+   <p>After losing the toss and being put in to bat, Zimbabwe captain Craig Ervine got his side off to a rollicking start,
+     as together with Regis Chakabva he took the hosts past fifty in the space of just 21 balls. The partnership ended at 51,
+      however, and Ervine in part to blame for luring his partner down the track with a stuttered start but Janak Prakash deserved 
+      most of the credit for a calm pick up in his follow-through and direct hit to run out Chakabva for a 10 ball 23.
+    </p>
+
+<h2>Chamdimal piles on the runs; Sri Lanka cement strong position</h2>
+
+<p>Sri Lanka built on their overnight lead steadily through the morning on the fourth day with Dinesh Chandimal getting to an unbeaten 153.
+     Chandimal also added 68 with Ramesh Mendis for the seventh wicket that frustrated Australia for a large part of the fourth morning.
+</p>
+
+<p>Both Chandimal and Mendis were proactive in their approach to counter Nathan Lyon's offspin. 
+    While the former brought out a string of sweep shots, the latter also put the spinner off by dancing down the track and lofting him.
+     As a result, Australia had little to no success through the first hour and Sri Lanka stretched their lead swiftly
+</p>
+
+<p>With no luck going their way, Australia only managed to get back into the game when the third new ball was taken. 
+    Ramesh was yorked by Mitchell Starc and trapped plumb in front. This came in the middle of a flurry of opportunities for Australia.
+     A couple of overs before, Mitchell Swepson had an LBW turned down, only for the replays to show three reds. With no review left, 
+     Australia were left ruing a missed chance when Starc nearly struck twice. 
+     After bagging Ramesh, he also had a caught behind appeal against Maheesh Theekshana upheld onfield but it was overturned on review.
+</p>
+
+<h1>Upcoming ICC Tournament</h1>
+
+  <table>
+    <tbody>
+        <tr>
+            <td><p><strong>TOURNAMENTS AND VENUES</strong></p>
+            <p><strong>2024 T20 World Cup:</strong>               USA, West Indies
+            </p><p><strong>2025 Champions Trophy:</strong>               Pakistan
+            </p><p><strong>2026</strong><strong>T20 World Cup:</strong>               India, Sri Lanka
+            </p><p><strong>2027 ODI World Cup:</strong>               South Africa, Zimbabwe, Namibia
+            </p><p><strong>2028 T20 World Cup:</strong>               Australia, New Zealand
+            </p><p><strong>2029 Champions Trophy:</strong>               India
+            </p><p><strong>2030 T20 World Cup:</strong>               England, Ireland, Scotland
+            </p><p><strong>2031 ODI World Cup:</strong>               India, Bangladesh
+            </p></td>
+        </tr>
+    </tbody>
+</table>
+
+<footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="container">
+        <span>Copyright © 2022 <a class="grey-text text-lighten-4" href="#" target="_blank">OCI</a> All rights reserved. Design and Developed by <a class="grey-text text-lighten-4" href="#">OCI</a></span>
+        
+      </div>
+    </div>
+</footer>
+
+<!-- partial -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.1/vue.min.js'></script><script  src="../script.js"></script>
+</body>
+</html>
