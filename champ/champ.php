51d14fc1de8cbde7defedb0af3796e4f361f8ac4
Ayush <119883228+Aayushh2108@users.noreply.github.com>
2023-04-25 22:26:37 +0530

    Add files via upload

---
+++ champ/champ.php
@@ -0,0 +1,162 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Champions Trophy</title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+
+    <link rel="stylesheet" href="../style2.css">
+</head>
+<body>
+  <header class="header">
+		<h1 class="logo"><a href="../welcome.php">OCI</a></h1>
+      <ul class="main-nav">
+          <li><a href="../welcome.php">Home</a></li>
+          <li><a href="../about.php">About</a></li>
+          <li><a href="../contact.php">Contact</a></li>
+          <li><a href="../logout.php">Logout</a></li>
+      </ul>
+	</header>
+  <h1 class="title">Champions Trophy</h1>
+
+  <h1>Icc Champions Trophy</h1>
+  <p>The ICC Champions Trophy is a One Day International (ODI) cricket tournament organised by the International Cricket Council (ICC). 
+    It was inaugurated as the ICC Knock Out Tournament in 1998 and has been played approximately every two years since. 
+    Its name was changed to the Champions Trophy in 2002. 
+    Since 2009, the tournament has only involved the eight highest-ranked ODI teams as of six months prior to the tournament.
+     The 8th edition of the ICC Champions Trophy is scheduled to be held in England and Wales between June 1-18, 2017.
+  </p>
+
+  <h1>Pakistan set to host 2025 Champions Trophy, India to co-host 2031 ODI World Cup with Bangladesh</h1>
+  <p>India is set to co-host the 2026 T20 World Cup with Sri Lanka and 2031 ODI World Cup with Bangladesh. It will also host the 2029 Champions Trophy.</p>
+
+  <h1>Champions Trophy Winners</h1>
+  <p>Champions Trophy, one of the most coveted cricket tournaments of the present times, has been played amongst the cricket-playing nations since the year 1998.
+     The fight for the championship takes place every two years, with the matches being spread out over a two-week period. Initially called the Knock Out Tournament,
+      its name was changed to ICC Champions Trophy in the year 2002 and it has been carried over ever since.
+
+After the first Champions Trophy of 1998, then called ICC Knock Out Tournament, South Africa emerged as the winner, beating West Indies by four wickets, in a match that was played in Dhaka, Bangladesh.
+ The latest Champions Trophy was held in the year 2006, with Australia being declared the winner. 
+It beat West Indies by eight wickets, to win its first ever Champions Trophy.
+
+In 2013 ICC Champions Trophy India beat host England by five runs. India and Australia won world second largest ODI tournament twise.
+ As for the winners in the interim championships, the names have been listed below.
+</p>
+
+<table class="stats-tbl">
+			<thead>
+				<tr><th>Year</th>
+					<th>Winner</th>
+					<th>Runner</th>
+					<th>Host</th>
+					<th>Format</th></tr>
+			</thead>
+			<tbody>
+				<tr><td>1998</td>
+					<td>South Africa</td>
+					<td>West Indies</td>
+					<td>Dhaka, Bangladesh</td>
+					<td>Knockout</td></tr>
+				<tr><td>2000</td>
+					<td>New Zealand</td>
+					<td>India</td>
+					<td>Nairobi, Kenya</td>
+					<td>Knockout</td></tr>
+				<tr><td>2002</td>
+					<td colspan="2">India and Sri Lanka (final was washed out)</td>
+					<td>Sri Lanka</td>
+					<td>Round Robin</td></tr>
+				<tr><td>2004</td>
+					<td>West Indies</td>
+					<td>England</td>
+					<td>England</td>
+					<td>Round Robin</td></tr>
+				<tr><td>2006</td>
+					<td>Australia</td>
+					<td>West Indies</td>
+					<td>India</td>
+					<td>Round Robin</td></tr>
+				<tr><td>2009</td>
+					<td>Australia</td>
+					<td>New Zealand</td>
+					<td>South Africa</td>
+					<td>Round Robin</td></tr>
+				<tr><td>2013</td>
+					<td>India</td>
+					<td>England</td>
+					<td>England</td>
+					<td>Round Robin</td></tr>
+				<tr><td>2017</td>
+					<td>Pakistan</td>
+					<td>India</td>
+					<td>England</td>
+					<td>Round Robin</td></tr>
+			</tbody>
+		</table>
+
+    <h1>2013 ICC Champions Trophy</h1>
+    <p>Won by  India <br>
+
+        Main article: 2013 ICC Champions TrophyEngland and Wales hosted the 2013 Champions Trophy.
+         England became the only country to host the Champions Trophy twice. Australia failed to win a single game in their group, and were knocked out along with New Zealand in Group A.
+          Pakistan lost all three games in Group B and were knocked out along with West Indies. England and Sri Lanka from Group A,
+           and India and South Africa from Group B, made it to the semi-finals.
+
+India and England won their respective games against Sri Lanka and South Africa comprehensively and the final between the two took place on 23 June 2013.
+ India beat England by 5 runs at Edgbaston, winning their second title, although their first title, in 2002, was shared with Sri Lanka due to the final being washed out.
+  Ravindra Jadeja was adjudged man of the match and he also received the "Golden Ball" for taking the most wickets in the tournament. 
+  Shikhar Dhawan received the "Golden Bat" for scoring the most runs in the series and was also adjudged the Man of the Series for his consistent outstanding performances. 
+MS Dhoni became the first captain in history to win all three major ICC trophies – World Cup in 2011, World T20 in 2007 and this edition of the Champions Trophy.</p>
+
+
+<h1>2017 ICC Champions Trophy</h1>
+
+<p>Won by  Pakistan <br>
+
+Main article: 2017 ICC Champions Trophy
+In the lead-up to the 2013 tournament, the ICC announced that the 2013 Champions Trophy was to be the last, with its place in the cricketing calendar to be taken by a new ICC World Test Championship.
+ However, in January 2014, that decision was reversed, due to the massive success of the 2013 edition, with the ICC confirming that the 2017 Champions Trophy tournament would take place and the proposed Test Championship was cancelled.
+  England and Wales hosted the 2017 ICC Champions Trophy. 
+  England became the only country to host the Champions Trophy thrice, and England and Wales became the only countries to host the ICC Champions Trophy consecutively, also hosting the 2013 edition. 
+  Bangladesh replaced the West Indies, who finished outside the top eight in ninth position, in the ICC ODI Team Rankings on the cut-off date.
+   Bangladesh returned to the ICC Champions Trophy for the first time since 2006, and, for the first time, the West Indies failed to qualify.
+
+Security around the tournament was increased following the Ariana Grande concert attack in Manchester, just before the start of the competition.
+ The International Cricket Council (ICC) announced that they would review security concerns. The 15 games played in the tournament were held across three venues – The Oval in London, Edgbaston Cricket Ground in Birmingham and Sophia Gardens in Cardiff. 
+ India did not announce their squad by the 25 April deadline due to what it described as "operational" reasons, although this was widely seen as a protest by the Board of Control for Cricket in India (BCCI) in an ongoing disagreement with the ICC over finance and governance.
+  After interference from senior officials, the Indian squad was finally named on 8 May 2017. Pakistan's Shoaib Malik played in his sixth consecutive Champions Trophy.</p>
+
+
+  <h1>2021 ICC Champions Trophy</h1>
+
+  <p>In the lead-up to the 2017 tournament, the ICC had proposed starting an ODI League in 2019, which will most likely lead to the Champions Trophy getting scrapped.
+   Following the 2017 Champions Trophy, David Richardson (the ICC CEO) stated that the future status of the Champions Trophy was undecided, with both a possible Test league and an additional World T20 putting additional pressure of fixtures.
+   In December 2017, the ICC's Future Tours Programme listed the 2021 edition taking place in India. However, in April 2018, the ICC announced that the tournament was scrapped, with the possibility of a T20 World Cup tournament replacing it. 2021 ICC T20 World Cup was originally due to be hosted in India, however, it was moved to UAE due to the COVID-19 pandemic.</p>
+
+   <h1>2025 ICC Champions Trophy</h1>
+   <p>On 16 November 2021, it was announced that the 2025 ICC Champions Trophy will be held in Pakistan.</p>
+
+   <footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="container">
+        <span>Copyright © 2022 <a class="grey-text text-lighten-4" href="#" target="_blank">OCI</a> All rights reserved. Design and Developed by <a class="grey-text text-lighten-4" href="#">OCI</a></span>
+        
+      </div>
+    </div>
+</footer>
+
+<!-- partial -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.1/vue.min.js'></script><script  src="../script.js"></script>
+</body>
+</html>
